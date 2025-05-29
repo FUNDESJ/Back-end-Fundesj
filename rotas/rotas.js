71f1ee80f749537f2cd55659ce646153a46eaae1
FUNDESJ <tecnologia@fundesj.com.br>
2025-05-29 11:55:54 -0300

    Api fundesj

--- rotas/rotas.js
+++ rotas/rotas.js
@@ -3,5 +3,5 @@ export const router = express.Router();
 import inscricaoRouter from './Inscricao.js';
 import certificadoRouter from './Certificado.js';
 
-router.use('/certificado', certificadoRouter);
-router.use('/palestras', inscricaoRouter);
+router.use('', certificadoRouter);
+router.use('', inscricaoRouter);
